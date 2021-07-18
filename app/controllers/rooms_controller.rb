f608f773cf9fa17f982ecc3ee3921557f0e96357
EC2 Default User <ec2-user@ip-172-31-12-150.us-east-2.compute.internal>
2021-07-18 07:02:31 +0000

    wip

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,50 @@
 class RoomsController < ApplicationController
+
+  def index
+    @rooms = Room.all
+  end
+
+  def show
+    @room = Room.find(params[:id])
+  end
+
   def new
     @room = Room.new
   end
+
+  def edit
+      @room = Room.find(params[:id])
+  end
+
+  def create
+    @room = Room.new(room_params)
+    @room.user = current_user
+
+    if @room.save
+      redirect_to @room, notice: "Room was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @room = Room.find(params[:id])
+    if @room.update(room_params)
+      redirect_to @room, notice: "Room was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @room = Room.find(params[:id])
+    @room.destroy
+    redirect_to rooms_url, notice: "Room was successfully destroyed."
+  end
+
+  private
+
+    def room_params
+      params.require(:room).permit(:title, :body)
+    end
 end
