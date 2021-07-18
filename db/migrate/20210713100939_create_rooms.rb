168852ba9397d331eb1808de4c3fe38ca7ae92ee
EC2 Default User <ec2-user@ip-172-31-12-150.us-east-2.compute.internal>
2021-07-18 06:33:15 +0000

    migration fileを修正

--- db/migrate/20210713100939_create_rooms.rb
+++ db/migrate/20210713100939_create_rooms.rb
@@ -1,7 +1,10 @@
 class CreateRooms < ActiveRecord::Migration[5.2]
   def change
     create_table :rooms do |t|
-
+      t.string "title"
+      t.text "body"
+      t.integer "user_id"
+    
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 ActiveRecord::Schema.define(version: 2021_07_13_100939) do
 
   create_table "rooms", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -23,9 +26,11 @@ ActiveRecord::Schema.define(version: 2021_07_13_100939) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
+    t.text "introduction"
+    t.string "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
